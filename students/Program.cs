fde77c527e93760b9a60dbf39e6c8ce2a8d77049
AbdurrahmanElrayes <32941712+AbdurrahmanElrayes@users.noreply.github.com>
2017-11-14 18:24:57 +0200

    Add files via upload

---
+++ students/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+namespace students
+{
+    class Program
+    {
+        static string topStudet(Dictionary<string,object> topDegrees){
+            string f = "";
+            int s=0;
+            foreach (var deg in topDegrees)
+            {
+                if ((int)deg.Value>s){
+                    s = (int)deg.Value;
+                    f = deg.Key;
+                }
+            }
+            return f;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the number of students");
+            int n = int.Parse(Console.ReadLine());
+            Dictionary<string,object> students = new Dictionary<string,object>();
+            for(int i = 0;i<n;i++){
+                Console.WriteLine("enter student "+(i+1)+" name");
+                string name = Console.ReadLine();
+                Console.WriteLine("enter his degree");
+                int deg = int.Parse(Console.ReadLine());
+                students.Add(name,deg);
+            }
+           aaa:Console.WriteLine("choose an option from the following menu\n====================================");
+            Console.WriteLine("[1] Search for student name and degree\n[2] Get top student name and degree\n[3] Change a student name");
+            int s = int.Parse(Console.ReadLine());
+            if(s==1){
+                    Console.WriteLine("please enter the student name");
+                    string a = Console.ReadLine();
+                    Console.WriteLine("student name: "+a+"\nstudent degree: "+students[a]);
+            }else if(s==2){
+                 Console.WriteLine("top student is: "+topStudet(students));
+            }else if (s==3){
+                Console.WriteLine("Please enter the student name");
+                string ch = Console.ReadLine();
+                object v = 0;
+                bool x = false;
+                foreach(var deg in students){
+                    if (deg.Key==ch){
+                        v = deg.Value;
+                        x = true;
+                    }
+                }
+                if (x==true){
+                    Console.WriteLine("Enter the new name");
+                students.Add(Console.ReadLine(), v);
+                }else {
+                    goto aaa;
+                }
+            }else{
+                Console.WriteLine("please enter a valid number");
+            }
+            goto aaa;
+        }
+    }
+}
